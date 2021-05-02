cd09bf0885aac046d22f1ec4c26eef025c3a0131
InKyoJeong <viapolar@naver.com>
2021-05-03 02:50:54 +0900

    Solve 21/05/03

---
+++ Inflearn-js/섹션6-자료구조/46_후위식연산.js
@@ -0,0 +1,53 @@
+// 후위식 연산 (postfix)
+function solution(s) {
+  let answer;
+  let stack = [];
+
+  for (let x of s) {
+    if (Number(x)) {
+      stack.push(x);
+    } else {
+      let rt = stack.pop();
+      let lt = stack.pop();
+      if (x === "+") {
+        stack.push(Number(lt) + Number(rt));
+      } else if (x === "-") {
+        stack.push(Number(lt) - Number(rt));
+      } else if (x === "*") {
+        stack.push(Number(lt) * Number(rt));
+      } else {
+        stack.push(Number(lt) / Number(rt));
+      }
+    }
+  }
+  answer = stack[stack.length - 1];
+
+  return answer;
+}
+
+let str = "352+*9-";
+console.log(solution(str));
+
+///////
+// Inf
+function solution(s) {
+  let answer;
+  let stack = [];
+  for (let x of s) {
+    if (!isNaN(x)) {
+      stack.push(Number(x));
+    } else {
+      let rt = stack.pop();
+      let lt = stack.pop();
+      if (x === "+") stack.push(lt + rt);
+      else if (x === "-") stack.push(lt - rt);
+      else if (x === "*") stack.push(lt * rt);
+      else if (x === "/") stack.push(lt / rt);
+    }
+  }
+  answer = stack[0];
+  return answer;
+}
+
+let str = "352+*9-";
+console.log(solution(str));
