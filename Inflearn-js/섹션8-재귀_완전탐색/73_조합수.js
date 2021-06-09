b0eeeba9bf17c538bbc8db5d6fc5e50e25360c3e
InKyoJeong <viapolar@naver.com>
2021-06-10 06:12:09 +0900

    Solve Inf 21/06/09

--- Inflearn-js/섹션8-재귀_완전탐색/73_조합수.js
+++ Inflearn-js/섹션8-재귀_완전탐색/73_조합수.js
@@ -1,19 +1,19 @@
 function solution(n, r) {
   let answer;
-  let memo = Array.from(Array(35), () => Array(35).fill(0));
+  //   let memo = Array.from(Array(35), () => Array(35).fill(0));
 
-  function DFS(n, r) {
-    if (memo[n][r] > 0) {
-      return memo[n][r];
-    }
-    if (n === r || r === 0) {
-      return 1;
-    } else {
-      return (memo[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r));
-    }
-  }
+  //   function DFS(n, r) {
+  //     if (memo[n][r] > 0) {
+  //       return memo[n][r];
+  //     }
+  //     if (n === r || r === 0) {
+  //       return 1;
+  //     } else {
+  //       return (memo[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r));
+  //     }
+  //   }
 
-  answer = DFS(n, r);
+  //   answer = DFS(n, r);
   return answer;
 }
 
