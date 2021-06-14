84e2d1b21b4ce7a562e440e1882e569fdc68a278
InKyoJeong <viapolar@naver.com>
2021-06-14 23:38:48 +0900

    Solve Inf 21/06/14

---
+++ Inflearn-js/섹션9-그래프와_탐색/81_이진트리넓이우선탐색.js
@@ -0,0 +1,19 @@
+function solution() {
+  let answer = "";
+  //   let queue = [];
+  //   queue.push(1);
+  //   while (queue.length) {
+  //     let v = queue.shift();
+  //     answer += v + " ";
+  //     for (let n of [v * 2, v * 2 + 1]) {
+  //       if (n > 7) {
+  //         continue;
+  //       }
+  //       queue.push(n);
+  //     }
+  //   }
+
+  return answer;
+}
+
+console.log(solution());
