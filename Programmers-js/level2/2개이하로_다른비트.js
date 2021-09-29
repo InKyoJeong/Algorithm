b626c0132a1a108ba717ad83d7e25a77a5c8a814
InKyoJeong <viapolar@naver.com>
2021-09-29 15:52:32 +0900

    solve(programmers): 2개 이하로 다른 비트

--- Programmers-js/level2/2개이하로_다른비트.js
+++ Programmers-js/level2/2개이하로_다른비트.js
@@ -47,3 +47,25 @@ function solution(numbers) {
 // 이 경우, 뒤에서 부터 첫번째로 등장하는 "01"을 찾는다.
 // 그리고 이 부분을 "10"으로 바꿔주면 2비트만 바꾸어 만들 수 있는 가장 작은 값이 된다.
 // ex: 11011(27) -> 11101(29)
+
+function solution(numbers) {
+  var answer = [];
+
+  numbers.forEach((number) => {
+    let str = "0" + number.toString(2);
+    if (str[str.length - 1] === "0") {
+      answer.push(number + 1);
+    } else {
+      let strArr = str.split("");
+      for (let i = strArr.length - 1; i >= 0; i--) {
+        if (strArr[i] === "0") {
+          strArr.splice(i, 2, "10");
+          break;
+        }
+      }
+      answer.push(parseInt(strArr.join(""), 2));
+    }
+  });
+
+  return answer;
+}
